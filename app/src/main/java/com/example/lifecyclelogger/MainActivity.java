ff4b0c94ebcf8e349193d55426f23873a25e21e0
adara-a11y <adar.a@a-y.org.il>
2026-02-06 10:57:31 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/lifecyclelogger/MainActivity.java
+++ app/src/main/java/com/example/lifecyclelogger/MainActivity.java
@@ -4,48 +4,44 @@ import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
-/**
- * Activity Lifecycle Logger - משימה 1
- * 
- * המשימה שלכם: הוסיפו הודעות Log.d() לכל פונקציות מחזור החיים.
- * 
- * פונקציות שצריך להוסיף:
- * 1. onCreate() - כבר קיימת, רק צריך להוסיף Log
- * 2. onStart()
- * 3. onResume()
- * 4. onPause()
- * 5. onStop()
- * 6. onDestroy()
- * 
- * דוגמה להודעת Log:
- * Log.d("LifecycleDemo", "onCreate called");
- */
 public class MainActivity extends AppCompatActivity {
 
-    // TAG לשימוש ב-Log - השתמשו בזה בכל ההודעות
-    private static final String TAG = "LifecycleDemo";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // TODO: הוסיפו כאן הודעת Log.d() שאומרת שנקראה onCreate
-        
+        Log.d("LifecycleDemo", "onCreate called");
     }
 
-    // TODO: הוסיפו את הפונקציה onStart() עם הודעת Log
-    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifecycleDemo", "onStart called");
+    }
 
-    // TODO: הוסיפו את הפונקציה onResume() עם הודעת Log
-    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifecycleDemo", "onResume called");
+    }
 
-    // TODO: הוסיפו את הפונקציה onPause() עם הודעת Log
-    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifecycleDemo", "onPause called");
+    }
 
-    // TODO: הוסיפו את הפונקציה onStop() עם הודעת Log
-    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifecycleDemo", "onStop called");
+    }
 
-    // TODO: הוסיפו את הפונקציה onDestroy() עם הודעת Log
-    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifecycleDemo", "onDestroy called");
+    }
 }
